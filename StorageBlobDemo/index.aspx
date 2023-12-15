a918c226a466bdd7c489a7525b4e317acafddcf6
Akshay K S <akspks88@gmail.com>
2023-12-15 22:48:39 +0530

    Add project files.

---
+++ StorageBlobDemo/index.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" Async="true" CodeBehind="index.aspx.cs" Inherits="StorageDemo.index" %>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table>
+                <tr>
+                    <td colspan="2"><h2>Microsoft Azure Storage Demo</h2></td>
+                </tr>
+                <tr>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td>Profile Photo:</td>
+                    <td><asp:FileUpload ID="photoUpload" runat="server" /></td>
+                </tr>
+                <tr>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td>&nbsp;</td>
+                    <td><asp:Button ID="btnInsert" Text="Insert" runat="server" OnClick="btnInsert_Click" /></td>
+                </tr>
+                <tr>
+                    <td>&nbsp;</td>
+                    <td><asp:Label ID="lblInsert" Text="" runat="server" /></td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
+
---
+++ StorageBlobDemo/index.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+namespace StorageDemo
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected async void btnInsert_Click(object sender, EventArgs e)
+        {
+            // Retrieve the storage account from the connection string.
+            CloudStorageAccount storageAccount = new CloudStorageAccount(new StorageCredentials("demosatcs1", "b8iN/AP3lmTYi8dhpMr2lnXKyWKO/8ePVrw6qhisX/2qbf/2WMWeStjmQ2Jt7w6J0UMTVO6BVf21+AStowksvw=="), true);
+
+            // Create the blob client
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            // Retrieve reference to a previously created container
+            CloudBlobContainer container = blobClient.GetContainerReference("images");
+            await container.CreateIfNotExistsAsync();
+
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference(photoUpload.PostedFile.FileName);
+
+            // Create or overwrite the "myblob" blob with contents from a local file.
+            using (var fileStream = photoUpload.PostedFile.InputStream)
+            {
+                blockBlob.UploadFromStream(fileStream);
+            }
+
+            lblInsert.Text = "Data Inserted Successfully!!!";
+        }
+    }
+}
